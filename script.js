49e11c80464878de5369df30cf1c5e676fee7622
shod-jpg <24a_shod@liceum.ztu.edu.ua>
2025-02-18 12:21:29 +0200

    кнопка

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("btn_LoveWishes");
+    const paragraph = document.getElementById("LoveWishes");
+    const texts = ["❤️Твій день буде сповнений щастям❤️", "❤️Тебе чекають великі пригоди❤️", "❤️Не здавайся, йди до цілі!❤️"];
+    let index = 0;
+    
+    button.addEventListener("click", function () {
+        index = (index + 1) % texts.length;
+        paragraph.textContent = texts[index];
+    });
+});
