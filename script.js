a8c71ca2f8c4643439044de631b2c81d2f3e42ab
shod-jpg <24a_shod@liceum.ztu.edu.ua>
2025-02-18 12:41:25 +0200

    Побажання

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const button = document.getElementById("btn_LoveWishes");
     const paragraph = document.getElementById("LoveWishes");
-    const texts = ["❤️Твій день буде сповнений щастям❤️", "❤️Тебе чекають великі пригоди❤️", "❤️Не здавайся, йди до цілі!❤️"];
+    const texts = ["❤️Твій день буде сповнений щастям❤️", "❤️Тебе чекають великі пригоди❤️", "❤️Не здавайся, йди до цілі!❤️", "❤️Любові та щастя тобі!❤️", "❤️Ти найкращий!❤️","❤️В тебе все вийде!❤️", "❤️Ти неповторний!❤️", "❤️Тебе люблять!❤️", "❤️Не сумуй, йди до цілі!❤️"];
     let index = 0;
     
     button.addEventListener("click", function () {
